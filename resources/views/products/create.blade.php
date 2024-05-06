54b645b6c94663e973dfc34d83aeafe14e8e012a
Tornike Epitashvili <epita97@gmail.com>
2024-05-06 15:11:58 +0400

    first commit

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Create a product</h1>
+<div>
+    @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+
+            @endforeach
+
+
+        </ul>
+
+    @endif
+</div>
+<form method="post" action="{{route('product.store')}}">
+    @csrf
+    @method('post')
+    <div>
+        <label>Name</label>
+        <input type="text" name="name" placeholder="Name">
+    </div>
+    <div>
+        <label>Qty</label>
+        <input type="number" name="qty" placeholder="Qty">
+    </div>
+    <div>
+        <label>Price</label>
+        <input type="text" name="price" placeholder="Price">
+    </div>
+    <div>
+        <label>Describtion</label>
+        <input type="text" name="description" placeholder="Description">
+    </div>
+
+    <div>
+        <input type="submit" value="Save as New Prouct">
+    </div>
+</form>
+</body>
+</html>
